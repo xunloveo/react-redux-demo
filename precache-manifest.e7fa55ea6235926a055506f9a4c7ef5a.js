f462d33ac72f3e1cb96c2e95cc8e41eca4f4b291
xunloveo <1285302438@qq.com>
2019-08-12 13:47:05 +0800

    完整版

---
+++ precache-manifest.e7fa55ea6235926a055506f9a4c7ef5a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d606e7982ca43bf7ceae9b906c14a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1785490cdf6556c997dc",
+    "url": "./static/css/3.de2dee3e.chunk.css"
+  },
+  {
+    "revision": "3a470c0792e8deccbf30",
+    "url": "./static/css/4.14b4ac99.chunk.css"
+  },
+  {
+    "revision": "6ddf6416d24adb4d05d2",
+    "url": "./static/css/5.fe9b6460.chunk.css"
+  },
+  {
+    "revision": "e118c6b792f62d891d8a",
+    "url": "./static/css/main.db3ac409.chunk.css"
+  },
+  {
+    "revision": "c4ba9f31b3f05ea24b55",
+    "url": "./static/js/2.3d325127.chunk.js"
+  },
+  {
+    "revision": "1785490cdf6556c997dc",
+    "url": "./static/js/3.9e7cea2d.chunk.js"
+  },
+  {
+    "revision": "3a470c0792e8deccbf30",
+    "url": "./static/js/4.ab635c95.chunk.js"
+  },
+  {
+    "revision": "6ddf6416d24adb4d05d2",
+    "url": "./static/js/5.91e0da80.chunk.js"
+  },
+  {
+    "revision": "e118c6b792f62d891d8a",
+    "url": "./static/js/main.0c7acc2a.chunk.js"
+  },
+  {
+    "revision": "d151e3cd1118b3276ea0",
+    "url": "./static/js/runtime~main.c91e3804.js"
+  },
+  {
+    "revision": "5c25e1cb9735adf1b556f42b8df9b7d1",
+    "url": "./static/media/iconfont.5c25e1cb.svg"
+  },
+  {
+    "revision": "7e008a771d5079e5051b632adcff1243",
+    "url": "./static/media/iconfont.7e008a77.eot"
+  },
+  {
+    "revision": "d828102abc66b0a0a7cd5eb3f9b4e2b4",
+    "url": "./static/media/iconfont.d828102a.ttf"
+  }
+]);
--- src/store/production/action-type.js
+++
@@ -1,8 +0,0 @@
-// 获取商品
-export const GETPRODUCTION = 'GETPRODUCTION'
-// 选择商品
-export const TOGGLESELECT = 'TOGGLESELECT'
-// 编辑商品
-export const EITPRODUCTION = 'EITPRODUCTION'
-// 清空选择
-export const CLEARSELECTED = 'CLEARSELECTED'
